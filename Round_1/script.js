aad8916341f68a7b9d20aa9a15df0aba736da613
Aravind Swami <aravindswami135@gmail.com>
2022-11-06 00:06:56 +0530

    Base

---
+++ Round_1/script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  Particles.init({
+    selector: ".background"
+  });
+};
+const particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 50,
+        connectParticles: true,
+        move: false,
+      }
+    }
+  ]
+});
